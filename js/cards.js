7b1e55a37fcee9744390c2be0e678328cb93bb9f
Oleksii Mits <oleksii.mits@gmail.com>
2020-01-29 00:16:54 +0200

    Trigger modal form

--- js/cards.js
+++ js/cards.js
@@ -6,3 +6,25 @@ import VisitDentist from './visit-dentist.js';
 class Planner {
 
 }
+
+
+let cardModal = document.querySelector('.container-item__modal');
+let cardTriggerBtn = document.querySelector('.container-item__header-add-card');
+let cardCloseBtn = document.querySelector('.container-item__modal-close');
+let cardForm = document.querySelector('.container-item__modal-form');
+
+cardTriggerBtn.addEventListener('click', () => {
+    cardModal.style.display = "block";
+});
+cardCloseBtn.addEventListener('click', () => {
+    cardModal.style.display = "none";
+    cardForm.reset();
+})
+
+window.addEventListener('click', function(event){
+    console.log(event);
+    if (event.target == cardModal) {
+        cardModal.style.display = "none";
+        cardForm.reset();
+    }
+})
