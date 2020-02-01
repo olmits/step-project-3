abd6011102f899c279d90d94da6d51a712396e5a
Oleksii Mits <oleksii.mits@gmail.com>
2020-02-01 15:30:49 +0200

    Form Data to JSON

--- js/cards.js
+++ js/cards.js
@@ -5,9 +5,9 @@ import VisitDentist from './visit-dentist.js';
 const addCard = document.querySelector('.container-item__header-add-card');
 
 const cardModal = document.querySelector('.container-item__modal');
-const cardForm = cardModal.querySelector('.container-item__modal-form');
-const cardCloseBtn = cardModal.querySelector('.container-item__modal-close');
-const cardFormSelect = cardForm.querySelector('.container-item__modal-form-select');
+const cardForm = document.querySelector('.container-item__modal-form');
+const cardCloseBtn = document.querySelector('.container-item__modal-close');
+const cardFormSelect = document.querySelector('.container-item__modal-form-select');
 
 addCard.addEventListener('click', () => {
     const modalProcessing = new Modal(cardModal, cardForm, cardCloseBtn, cardFormSelect);
@@ -23,13 +23,13 @@ class Modal {
         this.closeBtn = closeBtn;
         this.formSelector = formSelector;
         this.patientField = this.form.querySelector('.patient-info-field');
-        this.submitBtn = this.form.querySelector('.container-item__modal-form-submit')
     }
 
     openModal = () => {
         this.modal.style.display = "block";
         this._handleSelect();
         this._handleModalClosing(); 
+        this._handleFormSubmit();
     }
     closeModal = (t, event) => {
         if (event.target == t) {
@@ -38,40 +38,59 @@ class Modal {
             this._removeListeners();
         }
     }
-
-    proceedSubmit() {
-        this.submitBtn.addEventListener('click',)
+    
+    _formToJSON() {
+        let data = {content: {}};
+        for (let index = 0; index < this.form.elements.length; index++) {
+            let el = this.form.elements[index]
+            if (!('target' in el.dataset)) continue
+            (el.dataset.target === 'general-info' ? data[el.name] = el.value : data.content[el.name] = el.value);
+        }
+        return data;
+    }
+    proceedSubmit(event) {
+        event.preventDefault();
+        let userData = this._formToJSON();
+        console.log(userData);
+        
+        let userDataJson = JSON.stringify(userData);
+        console.log(userDataJson);
+        
+        
     }
     proceedSelect = (event) => {
         this.formSelector.querySelector('option[value=""]').disabled = true;
         this.form.dataset.medicineType = event.target.value;
         
         this.patientField.innerHTML = "";
-        this.patientField.append(this._createPatientInput('patient-name', 'text', 'container-item__modal-form-name'));
-        this.patientField.append(this._createPatientInput('visit-purpose', 'text', 'container-item__modal-form-purpose'));
-
+        this.patientField.append(this._createPatientInput('title', 'text', 'general-info'));
+        
         switch (event.target.value) {
             case 'therapist':
-                this.patientField.append(this._createPatientInput('patient-age', 'number', 'container-item__modal-form-age'));
+                this.patientField.append(this._createPatientInput('patient-name', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('age', 'text', 'user-content'));
                 break;
             case 'cardiologist':
-                this.patientField.append(this._createPatientInput('blood-pressure', 'text', 'container-item__modal-form-bp'));
-                this.patientField.append(this._createPatientInput('body-mass-index', 'text', 'container-item__modal-form-bmi'));
-                this.patientField.append(this._createPatientInput('past-medical-history', 'text', 'container-item__modal-form-pmh'));
-                this.patientField.append(this._createPatientInput('patient-age', 'number', 'container-item__modal-form-age'));
+                this.patientField.append(this._createPatientInput('patient-name', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('bp', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('weight', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('heartIllness', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('age', 'text', 'user-content'));
                 break;
             case 'dentist':
-                this.patientField.append(this._createPatientInput('last-visit', 'date', 'container-item__modal-form-date'));
+                this.patientField.append(this._createPatientInput('patient-name', 'text', 'user-content'));
+                this.patientField.append(this._createPatientInput('dateOfLastVisit', 'text', 'user-content'));
                 break;
-        }
+            }
     }
-    _createPatientInput(name, type, className) {
+    _createPatientInput(name, type, dataTarget) {
         let patientInput = document.createElement('input');
 
-        patientInput.className = className
+        patientInput.dataset.target = dataTarget;
         patientInput.setAttribute('name', name);
         patientInput.setAttribute('type', type);
         patientInput.setAttribute('placeholder', name[0].toUpperCase() + name.substring(1).toLowerCase().replace('-', ' '));
+        patientInput.value = name
         patientInput.required= true
 
         return patientInput
@@ -82,6 +101,10 @@ class Modal {
         this.patientField.innerHTML = "";
         this.form.dataset.medicineType ="";
         this.form.reset();
+    }
+    _handleFormSubmit() {
+        this.proceedSubmit = this.proceedSubmit.bind(this);
+        this.form.addEventListener('submit', this.proceedSubmit);
     }
     _handleModalClosing() {
         this.closeModalButton = this.closeModal.bind(this, this.closeBtn)
@@ -95,6 +118,7 @@ class Modal {
         this.formSelector.addEventListener('change', this.proceedSelect);
     }
     _removeListeners() {
+        this.form.removeEventListener('submit', this.proceedSubmit);
         this.formSelector.removeEventListener('change', this.proceedSelect);
         this.closeBtn.removeEventListener('click', this.closeModalButton);
         window.removeEventListener('click', this.closeModalWindow);
