7efd3a7defe20eb4349669561e3307f3b784bb93
Valeriia Muravska <valeriia.muravska@wirexapp.com>
2020-02-04 18:36:33 +0200

    update

--- js/action-with-cards.js
+++ js/action-with-cards.js
@@ -94,8 +94,8 @@ async function init() {
     // await console.log('GET_ALL_CARDS', respGetCards);
     // const respGetCard = await test.getCard(3063);
     // await console.log('GET_CARD',respGetCard);
-    // const respPostCard = await test.createCard(dentist);
-    // await console.log('POST_CARD',respPostCard);
+    const respPostCard = await test.createCard(dentist);
+    await console.log('POST_CARD',respPostCard);
     // const respPutCard = await test.updateCard(cardiologist, 3411);
     // await console.log('PUT_CARD',respPutCard);
     // const respDeleteCard = await test.deleteCard(3501);
